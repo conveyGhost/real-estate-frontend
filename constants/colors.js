be4b1cc383ff6b057042a4b1233d94e6a6526a0a
conveyGhost <glide.glister0326@gmail.com>
2018-04-10 03:38:49 +0200

    colorjs added

---
+++ constants/colors.js
@@ -0,0 +1,33 @@
+export const text = '#262626'
+
+export const blue = {
+  light: '#e1f7ff',
+  medium: '#227ea2',
+  dark: '#1d6c8b'
+}
+
+export const red = {
+  light: '#fcf0f0',
+  medium: '#e54848',
+  dark: '#902020'
+}
+
+export const orange = {
+  light: '#ffe3cb',
+  medium: '#ff881d'
+}
+
+export const green = {
+  light: '#e6ffed',
+  medium: '#0dbd62',
+  border: '#008647',
+  dark: '#00ac54'
+}
+
+export const offWhite = '#eee'
+export const lightestGray = '#dcdcdc'
+export const lightGray = '#bbb'
+export const mediumGray = '#bababa'
+export const mediumDarkGray = '#8c8c8c'
+export const gray4a = '#4a4a4a'
+export const grayf0 = '#f0f0f0'
